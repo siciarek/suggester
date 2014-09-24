84573f8d880570961db793f5cb459972e9428328
Jacek Siciarek <siciarek@wp.pl>
2014-09-24 09:22:16 +0200

    Add i18n switch controller

---
+++ app/modules/common/models/plugins/SecurePlugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: siciarek
+ * Date: 23.09.14
+ * Time: 08:05
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Application\Common\Plugin;
+
+
+class SecurePlugin {
+
+
+    public function __construct() {
+
+    }
+}
